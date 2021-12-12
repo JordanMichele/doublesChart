39cdeb52be567c872aa6dad0f78284440fc39d41
Jordan Michele <jordanmichele32@gmail.com>
2021-12-12 15:38:34 -0500

    Update scapers.js

--- scapers.js
+++ scapers.js
@@ -23,10 +23,6 @@ app.use(limiter);
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, './charting-frontend/build')));
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-    app.use(express.static(path.resolve(__dirname, './charting-frontend/build', 'index.html')));
-});
 
 app.post('/chart', async function(req,res){
     try{
