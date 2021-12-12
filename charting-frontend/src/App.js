677b7a3fd00ba26d893930f9c4081572edd47a9c
Jordan Michele <jordanmichele32@gmail.com>
2021-12-12 16:47:56 -0500

    Update App.js

--- charting-frontend/src/App.js
+++ charting-frontend/src/App.js
@@ -67,7 +67,7 @@ class App extends Component{
       loading: true
     });
      let urlEncoded = 'url='+this.state.url+'&fNum='+this.state.firstRace+'&sNum='+this.state.secondRace;
-     fetch('https://doubles-chart.herokuapp.com/chart', {
+     fetch('/api/chart', {
       method: 'POST',
       mode: 'cors', // this cannot be 'no-cors'
       headers: {
