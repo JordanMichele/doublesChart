d0d409dd77787b2165df53269d0ef3bd31f8109c
Jordan Michele <jordanmichele32@gmail.com>
2022-01-14 14:09:56 -0500

    Update scapers.js

--- scapers.js
+++ scapers.js
@@ -74,7 +74,7 @@ async function scrapeProduct(url){
         17 : []
     };
 
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: false });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
     const page = await browser.newPage();
     await page.goto(url, {
         waitUntil: 'load',
