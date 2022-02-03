3af1458b9b5aba06d54c45b6f934fa68ceff01e9
Jordan Michele <jordanmichele32@gmail.com>
2022-02-03 16:39:06 -0500

    Update scapers.js

--- scapers.js
+++ scapers.js
@@ -9,23 +9,23 @@ const rateLimit     = require('express-rate-limit');
 const path          = require('path');
 const cors          = require('cors');
 const PORT          = process.env.PORT || 3001;
-const cluster       = require("cluster");
-const totalCPUs     = require("os").cpus().length;
+// const cluster       = require("cluster");
+// const totalCPUs     = require("os").cpus().length;
 
-if (cluster.isMaster) {
-    console.log(`Number of CPUs is ${totalCPUs}`);
-    console.log(`Master ${process.pid} is running`);
-    // Fork workers.
-for (let i = 0; i < totalCPUs; i++) {
-    cluster.fork();
-}
+// if (cluster.isMaster) {
+//     console.log(`Number of CPUs is ${totalCPUs}`);
+//     console.log(`Master ${process.pid} is running`);
+//     // Fork workers.
+// for (let i = 0; i < totalCPUs; i++) {
+//     cluster.fork();
+// }
       
-cluster.on("exit", (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
-    console.log("Let's fork another worker!");
-    cluster.fork();
-    });
-} else { 
+// cluster.on("exit", (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died`);
+//     console.log("Let's fork another worker!");
+//     cluster.fork();
+//     });
+// } else { 
 
 const app = express();
 puppeteer.use(StealthPlugin());
@@ -132,4 +132,4 @@ app.listen(PORT, () => {
     console.log(`ON PORT ${PORT}`);
 });
 	
-}
+// }
